8b5f7c74983df5dc4b4bc0fe0c4f6fb66d5e9a93
DemonQilin <velezloaizaesteban9012015@gmail.com>
2022-07-02 11:51:22 -0500

    Se creó una nueva variable de estado que referencia el filtro aplicado a los pokemons

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import userName from './reducers/userName';
 import pokemons from './reducers/pokemons.slice';
 import viewPokemons from './reducers/viewPokemons.slice';
+import typesPokemons from './reducers/typesPokemons.slice';
+import filterPokemons from './reducers/filterPokemons.slice';
 
 export default configureStore({
     reducer: {
         userName,
         pokemons,
-        viewPokemons
+        viewPokemons,
+        typesPokemons,
+        filterPokemons,
     }
 })
